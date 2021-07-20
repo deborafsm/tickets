9b391e9dd10a1485552ed203e60efab14de8682a
deborafsm <deborafreiresoares@outlook.com>
2021-07-20 14:14:57 -0300

    Formulario

--- projeto_tickes/handlebars.js
+++ projeto_tickes/handlebars.js
@@ -6,14 +6,18 @@ const handlebars = require('express-handlebars');
     //Template Engine
     app.engine('handlebars',handlebars({defaultLayout:'main'}))
     app.set('view engine','handlebars');
-
-app.listen(8081, function(){
-    console.log("Servidor rodado na url https://localhost:8081");
-
-});
 //Conexão com o banco de dados MySql
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('projeto_chamado','root','',{
     host: 'localhost',
     dialect: 'mysql'
 })
+//Rotas
+        app.get('/cad',function(req,res){
+            res.render('formulario')
+        })
+
+app.listen(8081, function(){
+    console.log("Servidor rodado na url http://localhost:8081");
+
+});
